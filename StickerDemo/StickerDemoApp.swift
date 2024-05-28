aeea403f6c46d0ab02958fc171d00f1c34d7579d
Saboor Ahmad <saboorahm7e@gmail.com>
2024-05-28 19:40:50 +0500

    Initial Commit

---
+++ StickerDemo/StickerDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  StickerDemoApp.swift
+//  StickerDemo
+//
+//  Created by Saboor on 28/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct StickerDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
